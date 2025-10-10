083d95a7c493332101c0847ef967aad1362491ac
tanka <sharma.tankanath@gmail.com>
2025-10-10 14:41:37 -0500

    Removing unused Imports.

--- src/main/java/com/ecommerce/project/model/Address.java
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -6,10 +6,6 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
-
-import java.util.ArrayList;
-import java.util.List;
 @Entity
 @Table(name = "addresses")
 @Data
