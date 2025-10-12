17d42e72565629c7c3fdd2cdd8bebcb17da3f024
tanka <sharma.tankanath@gmail.com>
2025-10-11 19:23:05 -0500

    Getting Models, DTO and repository for Order, OrderItems and Payment

--- src/main/java/com/ecommerce/project/repositories/PaymentRepository.java
+++ src/main/java/com/ecommerce/project/repositories/PaymentRepository.java
@@ -1,7 +1,5 @@
 package com.ecommerce.project.repositories;
 
-import com.ecommerce.project.model.Order;
-import com.ecommerce.project.model.OrderItem;
 import com.ecommerce.project.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
